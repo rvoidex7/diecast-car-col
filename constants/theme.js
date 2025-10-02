fc97d6ef21da8fc39f33034c5312fd7df23a57c7
rvoidex7 <70305889+yakupozcan@users.noreply.github.com>
2025-10-02 22:01:50 +0300

    feat: add nativewind and tailwindcss for styling, update theme constants
    - Added nativewind dependency to package.json for utility-first styling.
    - Included tailwindcss in devDependencies for enhanced styling capabilities.
    - Created a new theme.js file to define color and size constants for consistent styling.
    - Added babel.config.js to configure Babel with nativewind plugin.
    - Set up tailwind.config.js to specify content paths for Tailwind CSS.

---
+++ constants/theme.js
@@ -0,0 +1,21 @@
+export const COLORS = {
+  primary: '#FF6347',    // Turuncu-Kırmızı (Hot Wheels Rengi gibi)
+  secondary: '#444262',  // Koyu Gri
+  tertiary: '#FF7754',
+
+  gray: '#83829A',
+  gray2: '#C1C0C8',
+
+  white: '#F3F4F8',
+  lightWhite: '#FAFAFC',
+  black: '#000000',
+};
+
+export const SIZES = {
+  xSmall: 10,
+  small: 12,
+  medium: 16,
+  large: 20,
+  xLarge: 24,
+  xxLarge: 32,
+};
